d2f15fef9d6832ac4695ae6961347267d052f3d7
DEVOPS\Jason <romeroagency@outlook.com>
2018-03-14 06:19:00 -0400

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=streamster-test-Web-Deploy.pubxml -p:Password=P9pNajGCuYRriauxNJ5GXoEyd9FdXljFt3Dv5tf4aCkHM7865q5NbuR7YMeM
+
